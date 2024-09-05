7d92ab1b8d20de8a657c534f4808291cce5ad22a
leehwan0218 <thhoney218@gmail.com>
2024-09-05 22:26:33 +1000

    Made CRUD system

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Home from './pages/Home';
 import AddUser from './users/AddUser';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Edituser from './users/Edituser';
+import ViewUser from './users/ViewUser';
 function App() {
   return (
     <div className="App">
@@ -14,6 +16,9 @@ function App() {
         <Routes>
           <Route exact path='/'element={<Home/>} />
           <Route exact path='/adduser'element={<AddUser/>} />
+          <Route exact path='/edituser/:id'element={<Edituser/>} />
+          <Route exact path='/viewuser/:id'element={<ViewUser/>} />
+
         </Routes>
       </Router>
    
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,48 +1,54 @@
 import React, { useEffect, useState } from 'react'
-import axios from  'axios'
+import axios from 'axios'
+import { Link, useParams } from 'react-router-dom';
+
 export default function Home() {
 
     const [users, setUsers] = useState([]);
+    const {id} = useParams()
 
-
-    useEffect(()=>{
+    useEffect(() => {
         loadUser();
-    },[])
+    }, [])
 
     const loadUser = async () => {
         const result = await axios.get("http://localhost:8080/users");
         setUsers(result.data);  // assuming result.data is the array of users
-      }
-      
-  return (
-    <div>
-        <table class="table">
-        <thead>
-            <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-            </tr>
-        </thead>
-        <tbody>
-            {users.map((user,index) => (
-                <tr>
-                <th scope="row" key={index}>{index +1 }</th>
-                <td>{user.id}</td>
-                <td>{user.name}</td>
-                <td>{user.email}</td>
-                <td>
-                    <button className='btn btn-primary mx-2'>View</button>
-                    <button className='btn btn-outline-primary mx-2'>Edit</button>
-                    <button className='btn btn-danger mx-2'>Delete</button>
+    }
 
-                </td>
-                </tr>
-            ))}
-           
-        </tbody>
-        </table>
-    </div>
-  )
+    const deleteUser = async (id) => {
+        await axios.delete(`http://localhost:8080/user/${id}`);
+        loadUser();
+    }
+
+    return (
+        <div>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((user, index) => (
+                        <tr key={user.id}> {/* Adding the unique key here */}
+                            <th scope="row">{index + 1}</th>
+                            <td>{user.name}</td>
+                            <td>{user.userName}</td>
+                            <td>{user.email}</td>
+                            <td>
+                                <Link className='btn btn-primary mx-2' to={`/viewuser/${user.id}`}>View</Link>
+                                <Link className='btn btn-outline-primary mx-2' to={`/edituser/${user.id}`}>Edit</Link>
+                                <button className='btn btn-danger mx-2' onClick={() => deleteUser(user.id)}>Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
