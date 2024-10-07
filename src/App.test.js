90889b74b9f03d58133b21f8cd9e1c02c88d108a
leehwan0218 <thhoney218@gmail.com>
2024-10-07 23:02:45 +1100

    ASD_backend Pipeline (for Backend Testing)

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import ProductPage from './ProductPage';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders product page', () => {
+    render(<ProductPage />);
+    const linkElement = screen.getByText(/Product Page/i);
+    expect(linkElement).toBeInTheDocument();
 });
