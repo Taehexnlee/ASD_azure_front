a9f383e383c4d389d3579a387460c93ee0c4a078
leehwan0218 <thhoney218@gmail.com>
2024-10-08 12:18:09 +1100

    frontend testing product page

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,12 @@
 module.exports = {
-    // Use babel-jest to transpile JavaScript files
     transform: {
-      '^.+\\.jsx?$': 'babel-jest',
+        '^.+\\.jsx?$': 'babel-jest',
     },
-    // Transforming node_modules containing ES6 code (like axios)
     transformIgnorePatterns: [
-      '/node_modules/(?!axios)/',
+        '/node_modules/(?!axios)/',
     ],
     moduleNameMapper: {
-      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     },
     testEnvironment: 'jsdom',
-  };
-  
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
-import ProductPage from './ProductPage';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import ProductPage from './pages/ProductPage';
+import { UserProvider } from './context/UserContext'; // Import the context provider
 
 test('renders product page', () => {
-    render(<ProductPage />);
+    render(
+        <UserProvider> {/* Wrap component in context provider */}
+            <ProductPage />
+        </UserProvider>
+    );
     const linkElement = screen.getByText(/Product Page/i);
     expect(linkElement).toBeInTheDocument();
 });
